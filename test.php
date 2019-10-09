7e74161efbf10a65b3dede44ecfe7253f89b8056
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2019-10-09 16:20:11 +0200

    Fix Style

--- test.php
+++ test.php
@@ -1,15 +1,17 @@
 <?php
 
+namespace App;
+
 class Test
 {
 
-    public function somefunction($foo, $bar,$baz)
+    public function somefunction($foo, $bar, $baz)
     {
-	$app->get('/hello/{name}', function ($name) use ($app) {
-    	    return 'Hello ' . $app->escape($name);
-	});
-	
-	return 'Hello World';
+        $app = new AppContainer();
+        $app->get('/hello/{name}', function ($name) use ($app) {
+            return 'Hello ' . $app->escape($name);
+        });
+
+        return 'Hello World';
     }
 }
-
