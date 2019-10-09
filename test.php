b7676720e28b14ac302ae3f256ad18e2cea55a6e
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2019-10-09 16:15:34 +0200

    Fix File

--- test.php
+++ test.php
@@ -3,12 +3,13 @@
 class Test
 {
 
-public function somefunction($foo, $bar, [
-  'test'
-], $baz);
-
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello ' . $app->escape($name);
-});
-
+    public function somefunction($foo, $bar,$baz)
+    {
+	$app->get('/hello/{name}', function ($name) use ($app) {
+    	    return 'Hello ' . $app->escape($name);
+	});
+	
+	return 'Hello World';
+    }
 }
+
