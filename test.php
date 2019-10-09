0bb7e38afdb83819b3165bbf62db9dd312fb5621
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2019-10-09 16:39:06 +0200

    Test Style

--- test.php
+++ test.php
@@ -2,14 +2,13 @@
 
 namespace App;
 
-class Test
+class TestClass
 {
-
     public function somefunction($foo, $bar, $baz)
     {
         $app = new AppContainer();
         $app->get('/hello/{name}', function ($name) use ($app) {
-            return 'Hello ' . $app->escape($name);
+            return 'Hello ' .$app->escape($name);
         });
 
         return 'Hello World';
