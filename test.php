dbbc7e4a4e7927094db0a5cbe27b4716aadcd105
Christian Schramm <cschra@hrz.tu-chemnitz.de>
2019-10-09 16:12:20 +0200

    Initial

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+class Test
+{
+
+public function somefunction($foo, $bar, [
+  'test'
+], $baz);
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+});
+
+}
